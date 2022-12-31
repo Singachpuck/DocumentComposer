3f308c286dcccd99b8f5a563b1f499cdc5a13450
Singachpuck <singachpucktv@gmail.com>
2022-12-31 21:50:55 +0200

    Repository init

---
+++ src/main/java/com/kpi/composer/DocumentComposerApplication.java
@@ -0,0 +1,13 @@
+package com.kpi.composer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentComposerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentComposerApplication.class, args);
+	}
+
+}
