bf550999f0f6bd88ecb0df996a450a79922e136c
Singachpuck <singachpucktv@gmail.com>
2023-11-22 22:24:09 +0100

    Spring Update: Remove old template UI

--- src/main/resources/static/js/send.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(() => {
-    $.getJSON('api/v1/users', (data) => {
-        for (let index in data) {
-            $('#users').append('<div><div>' + data[index].username + '</div><div>' + data[index].password + '</div></div>')
-        }
-    })
-});
